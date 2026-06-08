9d606e6c96f43fe0ae51ed59231a4eae54b82756
girraj-720 <girrajsinghsrcem91@gmail.com>
2026-06-09 02:59:22 +0530

    SOME RG DELETED

--- main.tf
+++ main.tf
@@ -2,29 +2,3 @@ resource "azurerm_resource_group" "RG1" {
   name     = "papaya-rg1"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "RG2" {
-  name     = "papaya-rg2"
-  location = "centralindia" 
-} 
-resource "azurerm_resource_group" "RG3" {
-  name     = "papaya-rg3"
-  location = "centralindia" 
-}
-resource "azurerm_resource_group" "RG3" {
-  name     = "papaya-rg3"
-  location = "West Europe" 
-}
-resource "azurerm_resource_group" "RG3" {
-  name     = "papaya-rg3"
-
-  location = "West Europe" 
-}
-resource "azurerm_resource_group" "RG3" {
-  name     = "papaya-rg3"
-  location = "central india" 
-}
-resource "azurerm_resource_group" "RG3" {
-  name     = "papaya-rg3"
-  location = "central india" 
-}
