fa422510a19d7fb0627cec2e8fdb87cbbcd3aef9
girraj-720 <gs9131015@gmail.com>
2026-06-09 01:37:38 +0530

    one more resource added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "RG1" {
   name     = "papaya-rg1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "RG2" {
+  name     = "papaya-rg2"
+  location = "West Europe"
+}
--- provider.tf
+++ provider.tf
@@ -9,4 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
+
+} # Configure the Microsoft Azure Provider
